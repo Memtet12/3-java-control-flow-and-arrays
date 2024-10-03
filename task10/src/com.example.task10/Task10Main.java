5fccb5cfbb263fb06c27919b65fba005b6b92c02
Memtet12 <eldotefc@gmail.com>
2024-10-04 01:24:24 +0500

    Дмитриев Андрей. Задание 10

--- task10/src/com.example.task10/Task10Main.java
+++ task10/src/com.example.task10/Task10Main.java
@@ -12,7 +12,14 @@ public class Task10Main {
 
     static int numMin(int[] arr) {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
-        return 0;
+        int index = 0;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            minValue =Math.min(minValue,arr[i]);
+            if (arr[i] == minValue) index = i;
+        }
+
+        return index;
     }
 
 }
